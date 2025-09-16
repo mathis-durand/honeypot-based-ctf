36aa82d78418978c5383bd4891ebf03a955a1fdd
mathis-durand <219549574+mathis-durand@users.noreply.github.com>
2025-09-16 12:25:02 +0200

    Update start.sh

--- deploy/lobby/start.sh
+++ deploy/lobby/start.sh
@@ -31,7 +31,6 @@ mkdir /run/sshd
 # Create required folder and start ssh server.
 mkdir -p /home/nobody1/.ssh
 cat /app/lobby/ssh-key-ctf.pub >> /home/nobody1/.ssh/authorized_keys
-cp /app/lobby/ssh-key-drone /home/nobody1/
 
 echo 'PermitRootLogin no' >> /etc/ssh/sshd_config # Not root login.
 # Permit password login
@@ -48,6 +47,7 @@ ssh-keygen -A
 
 # Keep the container running
 tail -f /dev/null
+
 
 
 
