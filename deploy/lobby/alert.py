8913f13cb95443ea0bbd2b8fdbbc1f1ccd42cdc5
mathis-durand <219549574+mathis-durand@users.noreply.github.com>
2025-09-17 12:05:46 +0200

    Update alert.py

--- deploy/lobby/alert.py
+++ deploy/lobby/alert.py
@@ -3,9 +3,10 @@ import os
 while True:
   if os.path.isfile("/msg/alert"):
     f = open("/msg/alert", "r")
-    msg = f.readline()
+    msg = f.readlines()
     f.close()
     os.remove("/msg/alert")
-    for line in msg.split("\n"):
+    for line in msg:
+      line=line.split("\n")[0]
       for pts in range(10): 
         os.system("echo \"" + line + "\" > /dev/pts/" + str(pts))
