932a1668e6ff6c23e383d2ce9f1023c2d81595cf
mathis-durand <219549574+mathis-durand@users.noreply.github.com>
2025-09-19 16:09:13 +0200

    Update alert.py

--- deploy/lobby/alert.py
+++ deploy/lobby/alert.py
@@ -6,7 +6,8 @@ while True:
     msg = f.readlines()
     f.close()
     os.remove("/msg/alert")
+    message = "\\n\\l"
     for line in msg:
-      line=line.split("\n")[0]
-      for pts in range(10): 
-        os.system("echo \"" + line + "\" > /dev/pts/" + str(pts))
+      message= message + line.split("\n")[0] + "\\n\\l"
+    for pts in range(10): 
+      os.system("printf \"" + message + "\" > /dev/pts/" + str(pts))
